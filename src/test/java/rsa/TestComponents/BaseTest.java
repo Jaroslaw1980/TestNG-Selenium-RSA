42815e1c4832ea50f9b7c715d7e4939d2a4ec21d
jnazar <jaroslawnazar@gmail.com>
2022-11-16 22:53:26 +0100

    refactored BaseTest class

--- src/test/java/rsa/TestComponents/BaseTest.java
+++ src/test/java/rsa/TestComponents/BaseTest.java
@@ -39,7 +39,13 @@ public class BaseTest {
                 getProperty("user.dir") + "/src/main/java/rsa/resources/GlobalData.properties");
         prop.load(file);
 
-        String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
+
+        String browserName;
+        if (System.getProperty("browser") != null) {
+            browserName= System.getProperty("browser");
+        } else {
+            browserName = prop.getProperty("browser");
+        }
 
         if (browserName.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
@@ -47,76 +53,76 @@ public class BaseTest {
 
         } else if (browserName.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+            driver = new FirefoxDriver();}
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().window().maximize();
+            driver.manage().deleteAllCookies();
+            return driver;
         }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        return driver;
+
+
+        public List<HashMap<String, String>> getJsonDataToMap (String path) throws IOException {
+
+            // read json to string
+            String jsonContent = FileUtils.readFileToString(new File(path), StandardCharsets.UTF_8);
+
+            //read string to hashmap
+            ObjectMapper mapper = new ObjectMapper();
+            List<HashMap<String, String>> data = mapper.
+                    readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {
+                    });
+            return data;
+        }
+
+        public String getScreenShoot (String testCaseName, WebDriver driver) throws IOException {
+            TakesScreenshot ts = (TakesScreenshot) driver;
+            File source = ts.getScreenshotAs(OutputType.FILE);
+            File file = new File("C:\\Projects\\SeleniumJava" + testCaseName + ".png");
+            FileUtils.copyFile(source, file);
+            return "C:\\Projects\\SeleniumJava" + testCaseName + ".png";
+
+        }
+
+        @BeforeMethod(alwaysRun = true)
+        public LandingPage launchLandingPageObject () {
+            this.landingPage = new LandingPage(driver);
+            return this.landingPage;
+        }
+
+        @BeforeMethod(alwaysRun = true)
+        public RegisterPage launchRegisterObject () {
+            this.registerPage = new RegisterPage(driver);
+            return this.registerPage;
+        }
+
+        @BeforeMethod(alwaysRun = true)
+        public ProductCataloge launchProductCatalogueObject () {
+            this.productCataloge = new ProductCataloge(driver);
+            return this.productCataloge;
+        }
+
+        @BeforeMethod(alwaysRun = true)
+        public CartPage launchCartPageObject () {
+            this.cartPage = new CartPage(driver);
+            return this.cartPage;
+        }
+
+        @BeforeMethod(alwaysRun = true)
+        public CheckoutPage launchCheckoutPageObject () {
+            this.checkoutPage = new CheckoutPage(driver);
+            return this.checkoutPage;
+
+        }
+
+        @BeforeMethod(alwaysRun = true)
+        public ConfirmationPage launchConfirmationPageObject () {
+            this.confirmationPage = new ConfirmationPage(driver);
+            return this.confirmationPage;
+        }
+
+        @AfterMethod(alwaysRun = true)
+        public void tearDown () {
+            driver.quit();
+        }
     }
-
-
-    public List<HashMap<String, String>> getJsonDataToMap(String path) throws IOException {
-
-        // read json to string
-        String jsonContent = FileUtils.readFileToString(new File(path), StandardCharsets.UTF_8);
-
-        //read string to hashmap
-        ObjectMapper mapper = new ObjectMapper();
-        List<HashMap<String, String>> data = mapper.
-                readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {
-                });
-        return data;
-    }
-
-    public String getScreenShoot(String testCaseName, WebDriver driver) throws IOException {
-        TakesScreenshot ts = (TakesScreenshot) driver;
-        File source = ts.getScreenshotAs(OutputType.FILE);
-        File file = new File("C:\\Projects\\SeleniumJava" + testCaseName + ".png");
-        FileUtils.copyFile(source, file);
-        return "C:\\Projects\\SeleniumJava" + testCaseName + ".png";
-
-    }
-
-    @BeforeMethod(alwaysRun = true)
-    public LandingPage launchLandingPageObject() {
-        this.landingPage = new LandingPage(driver);
-        return this.landingPage;
-    }
-
-    @BeforeMethod(alwaysRun = true)
-    public RegisterPage launchRegisterObject() {
-        this.registerPage = new RegisterPage(driver);
-        return this.registerPage;
-    }
-
-    @BeforeMethod(alwaysRun = true)
-    public ProductCataloge launchProductCatalogueObject() {
-        this.productCataloge = new ProductCataloge(driver);
-        return this.productCataloge;
-    }
-
-    @BeforeMethod(alwaysRun = true)
-    public CartPage launchCartPageObject() {
-        this.cartPage = new CartPage(driver);
-        return this.cartPage;
-    }
-
-    @BeforeMethod(alwaysRun = true)
-    public CheckoutPage launchCheckoutPageObject() {
-        this.checkoutPage = new CheckoutPage(driver);
-        return this.checkoutPage;
-
-    }
-
-    @BeforeMethod(alwaysRun = true)
-    public ConfirmationPage launchConfirmationPageObject() {
-        this.confirmationPage = new ConfirmationPage(driver);
-        return this.confirmationPage;
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() {
-        driver.quit();
-    }
-}
