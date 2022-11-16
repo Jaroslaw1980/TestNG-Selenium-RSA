5c2dd48647d25a8a8a0661450771dd57c552a22f
jnazar <jaroslawnazar@gmail.com>
2022-11-16 14:00:56 +0100

    Base Test refactor

--- src/test/java/rsa/Tests/ErrorLoginTests.java
+++ src/test/java/rsa/Tests/ErrorLoginTests.java
@@ -6,7 +6,7 @@ import rsa.TestComponents.BaseTest;
 
 public class ErrorLoginTests extends BaseTest {
     @Test(groups = {"ErrorHandling"})
-    public void validationTest(){
+    public void wrongPasswordTest(){
         this.landingPage.goTo();
         this.landingPage.loginUser("yaroBaro@gmail.com", "YaroBaro4$");
         Assert.assertEquals("Incorrect email or password.", this.landingPage.wrongPasswordMessage());
